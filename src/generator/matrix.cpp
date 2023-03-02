57ddd23ebb2994f667529cb9c2d0ac3cdfc535d9
mirelois <79458822+mirelois@users.noreply.github.com>
2023-03-02 14:00:36 +0000

    multiplicação de matrizes

---
+++ src/generator/matrix.cpp
@@ -0,0 +1,67 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+
+int matrix_mult(float* return_matrix[], float* matrix_1[], float* matrix_2[], int Row_1, int Col_1, int Row_2, int Col_2)
+{
+
+    if(Col_1 != Row_2){
+        return -1;
+    }
+
+    int i,j,k;
+
+    for(i = 0; i < Row_1; ++i)
+        for(j = 0; j < Col_2; ++j)
+        {
+            return_matrix[i][j]=0;
+        }
+
+    // Multiplying matrix a and b and storing in array return_matrix.
+    for(i = 0; i < Row_1; ++i)
+        for(j = 0; j < Col_2; ++j)
+            for(k = 0; k < Col_1; ++k)
+            {
+                return_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j];
+            }
+
+
+    return 0;
+}
+
+
+
+
+
+int main()
+{
+    float a1[] = {1,2};
+    float a2[] = {3,4};
+    float* a[] ={a1,a2};
+
+    float b1[] = {5,6};
+    float b2[] = {7,8};
+    float* b[] ={b1,b2};
+
+    float** return_matrix;
+
+    return_matrix = (float**)malloc(2);
+    for (int i=0; i<2; i++){
+        return_matrix[i] = (float*)malloc(2);
+    }
+
+    matrix_mult(return_matrix, a, b, 2, 2, 2, 2);
+
+    // Displaying the return_matrixiplication of two matrix.
+    cout << endl << "Output Matrix: " << endl;
+    for(i = 0; i < Row_1; ++i)
+    for(j = 0; j < Col_2; ++j)
+    {
+        cout << " " << return_matrix[i][j];
+        if(j == Col_2-1)
+            cout << endl;
+    }
+}
+
