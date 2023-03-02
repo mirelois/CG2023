63ab8cb4fd2efd2de096e2333b5316bb73abeb48
mirelois <79458822+mirelois@users.noreply.github.com>
2023-03-02 15:06:01 +0000

    header do matrix.cpp

--- src/generator/matrix.h
+++ src/generator/matrix.h
@@ -1,4 +1,6 @@
+#include <tuple>
 
 int matrix_mult(float* return_matrix[], float* matrix_1[], float* matrix_2[], int Row_1, int Col_1, int Row_2, int Col_2);
+std::tuple<float, float, float> matrix_mult_tuple(float* return_matrix[], float* matrix_1[], std::tuple<float, float, float> point, int Col_1);
 
 
