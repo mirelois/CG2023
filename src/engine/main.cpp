2cb666185547bc77381902bc9dcbd0ec8b2c6aaa
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2023-03-10 13:27:24 +0000

    working on linux

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -6,8 +6,7 @@ int main(int argc, char  *argv[]){
     Camera* camera = new Camera;
     Group* group = new Group;
     
-    char file[] = "phase1.xml";
-    parser(file, window, camera, group);
+    parser(argv[1], window, camera, group);
     run(window, camera, group, argc, argv);
     delete(window);
     delete(camera);
