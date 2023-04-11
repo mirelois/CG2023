a443ae4b347fbc84b141e3a2348d61feb8d9b43b
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2023-04-11 16:07:34 +0100

    cone

--- build/src/engine/CMakeFiles/engine.dir/DependInfo.cmake
+++ build/src/engine/CMakeFiles/engine.dir/DependInfo.cmake
@@ -14,8 +14,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/carlos/Documents/CG2023/src/engine/parser.cpp" "src/engine/CMakeFiles/engine.dir/parser.cpp.o" "gcc" "src/engine/CMakeFiles/engine.dir/parser.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/src/generator/CMakeFiles/generator.dir/DependInfo.cmake
+++ build/src/generator/CMakeFiles/generator.dir/DependInfo.cmake
@@ -9,11 +9,10 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/carlos/Documents/CG2023/src/generator/generator.cpp" "src/generator/CMakeFiles/generator.dir/generator.cpp.o" "gcc" "src/generator/CMakeFiles/generator.dir/generator.cpp.o.d"
-  "/home/carlos/Documents/CG2023/src/generator/matrix.cpp" "src/generator/CMakeFiles/generator.dir/matrix.cpp.o" "gcc" "src/generator/CMakeFiles/generator.dir/matrix.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
