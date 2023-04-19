1bf43b4815beb6910b7952a8ad944bfebb800f88
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2023-04-19 16:13:01 +0100

    Organizar melhor

--- src/generator/bezier.cpp
+++ src/generator/bezier.cpp
@@ -40,75 +40,66 @@ void parse_bezier(char *fileName, vector<vector<int>*>* patches, vector<vector<f
 
 }
 
-void matrixMultVector(float m[4][4], float v[4][1], float result[4][1]){ // Matrizes 4*4 e vetores 4
-    for(int i=0; i<4; i++){
-        result[i][0] = 0;
-        for(int j=0; j<4; j++){
-            result[i][0] += v[i][0]*m[i][j];
-        }
-    }
+
+void multMatrixVector(float *m, float *v, float *res) {
+	for (int j = 0; j < 4; ++j) {
+		res[j] = 0;
+		for (int k = 0; k < 4; ++k) {
+			res[j] += v[k] * m[j * 4 + k];
+		}
+	}
 }
 
-void calculate_points(vector<tuple<float,float,float>>* point_vector, vector<vector<int>*>* patches, vector<vector<float>>* cpoints, int tessellationLevel){
-    float MBezier[4][4] = {
+tuple<float,float,float> calculate_square(float u, float v, vector<int>* patch, vector<vector<float>>* cpoints){
+    float M[4][4] = { // Matriz de Bezier
     {-1, 3, -3, 1},
     {3, -6, 3, 0},
     {-3, 3, 0, 0},
     {1, 0, 0, 0}
     }; 
-    
-    for(vector<int>* patch: *patches){
-        printf("patch\n");
-            float point[3];
-            for(int i=0; i<=tessellationLevel; i++){
-                for(int j=0; j<=tessellationLevel; j++){
-                    for(int p = 0; p<3; p++){
-                    float MV[4][1]; // M^T = M no caso de Bezier
-                    float v[4][1] = {{j*j*j*1.0f/(tessellationLevel*tessellationLevel*tessellationLevel)},
-                     {j*j*1.0f/(tessellationLevel*tessellationLevel)},
-                     {j*1.0f/tessellationLevel},
-                      {1.0f}};
 
-                    matrixMultVector(MBezier, v, MV);
-                    
-                    float P[4][4] = {{cpoints->at(patch->at(0))[p], cpoints->at(patch->at(1))[p], cpoints->at(patch->at(2))[p], cpoints->at(patch->at(3))[p]},
-                                    {cpoints->at(patch->at(4))[p], cpoints->at(patch->at(5))[p], cpoints->at(patch->at(6))[p], cpoints->at(patch->at(7))[p]},
-                                    {cpoints->at(patch->at(8))[p], cpoints->at(patch->at(9))[p], cpoints->at(patch->at(10))[p], cpoints->at(patch->at(11))[p]},
-                                    {cpoints->at(patch->at(12))[p], cpoints->at(patch->at(13))[p], cpoints->at(patch->at(14))[p], cpoints->at(patch->at(15))[p]}
-                    };
+    float points[3];
+    for(int p=0; p<3; p++){
+        float V[4] = {v*v*v, v*v, v, 1};
+        float MV[4];
+        multMatrixVector(*M,V,MV);
 
-                    float PMV[4][1];
-                    matrixMultVector(P, MV, PMV);
+        float PMV[4];
+        float P[4][4] = {{cpoints->at(patch->at(0))[p], cpoints->at(patch->at(1))[p], cpoints->at(patch->at(2))[p], cpoints->at(patch->at(3))[p]},
+                        {cpoints->at(patch->at(4))[p], cpoints->at(patch->at(5))[p], cpoints->at(patch->at(6))[p], cpoints->at(patch->at(7))[p]},
+                        {cpoints->at(patch->at(8))[p], cpoints->at(patch->at(9))[p], cpoints->at(patch->at(10))[p], cpoints->at(patch->at(11))[p]},
+                        {cpoints->at(patch->at(12))[p], cpoints->at(patch->at(13))[p], cpoints->at(patch->at(14))[p], cpoints->at(patch->at(15))[p]}
+        };
 
-                    //printf("%f %f %f \n", PMV[0], PMV[1], PMV[2]);
+        multMatrixVector(*P, MV, PMV);
 
-                    float MPMV[4][1];
-                    matrixMultVector(MBezier, PMV, MPMV);
+        float MPMV[4];
+        multMatrixVector(*M, PMV, MPMV);
 
-                    //printf("%f %f %f \n", MPMV[0], MPMV[1], MPMV[2]);
-
-                    point[p] = (i*i*i)/(tessellationLevel*tessellationLevel*tessellationLevel)*MPMV[0][0] + (i*i)/(tessellationLevel*tessellationLevel)*MPMV[1][0] + i/tessellationLevel*MPMV[2][0] + MPMV[3][0];
-                    }
-                point_vector->push_back(make_tuple(point[0], point[1], point[2]));
-                printf("%f %f %f \n", point[0], point[1], point[2]);
-                }
-            
-        } 
+        points[p] = u*u*u*MPMV[0] + u*u*MPMV[1] + u*MPMV[2] + MPMV[3];
     }
+
+    return make_tuple(points[0], points[1], points[2]);
 }
 
-vector<tuple<float,float,float>>* generate_bezier(char *fileName, int tessellationLevel){
+vector<tuple<float,float,float>>* generate_bezier(char *file_name, int tessellation_level){
     vector<tuple<float,float,float>>* point_vector = new vector<tuple<float,float,float>>();
 
     vector<vector<int>*>* patches = new vector<vector<int>*>();
     vector<vector<float>>* cpoints = new vector<vector<float>>();
 
-    parse_bezier(fileName, patches, cpoints);
-    calculate_points(point_vector, patches, cpoints, tessellationLevel);
+    parse_bezier(file_name, patches, cpoints);
     
-    //for(tuple<float,float,float> tuplo: *point_vector){
-    //    printf("%f %f %f\n", get<0>(tuplo),get<1>(tuplo),get<2>(tuplo));
-    //}
-    
+    for(vector<int>* patch: *patches){
+        for(int u=0;u<=tessellation_level; u++){
+            for(int v=0; v<=tessellation_level; v++){
+                point_vector->push_back(calculate_square(u/tessellation_level*1.0f,v/tessellation_level*1.0f, patch, cpoints));
+                point_vector->push_back(calculate_square((u+1)/tessellation_level*1.0f,v/tessellation_level*1.0f, patch, cpoints));
+                point_vector->push_back(calculate_square((u+1)/tessellation_level*1.0f,(v+1)/tessellation_level*1.0f, patch, cpoints));
+                point_vector->push_back(calculate_square(u/tessellation_level*1.0f,(v+1)/tessellation_level*1.0f, patch, cpoints));
+            }
+        }
+    }
+
     return point_vector;
 }
