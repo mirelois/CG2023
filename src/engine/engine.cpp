46dfb5e8167a9218341ac650d1a481d33cfc8551
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-04-22 10:45:11 +0100

    Update engine.cpp

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -4,7 +4,7 @@ Camera* camera_global;
 Group* group_global;
 float  camera_move_delta = 1, look_rotate_delta_up = M_PI / 32, look_rotate_delta_right = M_PI / 32;
 int camera_side = 0, camera_up = 0, camera_front = 0, look_rotate_up = 0, look_rotate_right = 0;
-float saved[3];
+float last_camera_position[3];
 char axis = 1;
 char polygon = 1;
 
@@ -78,56 +78,12 @@ float normalize_vector(float p[3]) {
 	return norm;
 }
 
-void save_position() {
-	//os c�lculos est�o aqui dentro se por ventura nos interessar mudar a dire��o da c�mera (implica recalcular)
-	//se estiver demasiado lento p�e-se no in�cio
+
+void calculate_displacement(float displacement[3], float distance_to_lookat[3]) {
 	float d[3] = { camera_global->lookAt[0] - camera_global->position[0],
 		camera_global->lookAt[1] - camera_global->position[1],
 		camera_global->lookAt[2] - camera_global->position[2] };
 
-	normalize_vector(d);
-
-	//rota��o de l sobre o eixo up e sobre o eixo r
-	//na documenta��o do glut tem a matriz explicitada, ter cuidado que faltam parenteses
-	//https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/glRotate.xml
-
-	normalize_vector(camera_global->up);
-
-	rotate_over_vector(d, camera_global->up, look_rotate_right * look_rotate_delta_right);
-
-	//cross product � m�o enquanto n�o descubro como fazer pela fun��o cross (cad�?)
-	//a dire��o para a frente � o -z
-	float r[3] = { d[1] * camera_global->up[2] - camera_global->up[1] * d[2],
-		d[2] * camera_global->up[0] - camera_global->up[2] * d[0],
-		d[0] * camera_global->up[1] - camera_global->up[0] * d[1] };
-
-	normalize_vector(r);
-
-	rotate_over_vector(d, r, look_rotate_up * look_rotate_delta_up);
-
-	saved[0] += d[0] * camera_move_delta * camera_front + r[0] * camera_move_delta * camera_side + camera_up * camera_move_delta * camera_global->up[0];
-	saved[1] += camera_side * camera_move_delta * r[1] + camera_front * camera_move_delta * d[1] + camera_up * camera_move_delta * camera_global->up[1];
-	saved[2] += camera_side * camera_move_delta * r[2] + camera_front * camera_move_delta * d[2] + camera_up * camera_move_delta * camera_global->up[2];
-	camera_front = 0;
-	camera_up = 0;
-	camera_side = 0;
-}
-
-void renderScene(void) {
-
-	// clear buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// set the camera
-	glLoadIdentity();
-
-
-	//os c�lculos est�o aqui dentro se por ventura nos interessar mudar a dire��o da c�mera (implica recalcular)
-	//se estiver demasiado lento p�e-se no in�cio
-	float d[3] = { camera_global->lookAt[0] - camera_global->position[0],
-		camera_global->lookAt[1] - camera_global->position[1],
-		camera_global->lookAt[2] - camera_global->position[2] };
-	
 	float norm = normalize_vector(d);
 
 	//rota��o de l sobre o eixo up e sobre o eixo r
@@ -135,7 +91,7 @@ void renderScene(void) {
 	//https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/glRotate.xml
 
 	normalize_vector(camera_global->up);
-	
+
 	rotate_over_vector(d, camera_global->up, look_rotate_right * look_rotate_delta_right);
 
 	//cross product � m�o enquanto n�o descubro como fazer pela fun��o cross (cad�?)
@@ -150,15 +106,48 @@ void renderScene(void) {
 
 	//normalize_vector(d);
 
-	float desl[3] = { d[0] * camera_move_delta * camera_front + r[0] * camera_move_delta * camera_side ,
-		camera_side * camera_move_delta * r[1] + camera_front * camera_move_delta * d[1] + camera_up * camera_move_delta,
-		camera_side * camera_move_delta * r[2] + camera_front * camera_move_delta * d[2]
-	};
+	displacement[0] = d[0] * camera_move_delta * camera_front + r[0] * camera_move_delta * camera_side,
+	displacement[1] = camera_side * camera_move_delta * r[1] + camera_front * camera_move_delta * d[1] + camera_up * camera_move_delta,
+	displacement[2] = camera_side * camera_move_delta * r[2] + camera_front * camera_move_delta * d[2];
 
-	gluLookAt(	desl[0] + saved[0], desl[1] + saved[1], desl[2] + saved[2],
-				saved[0] + desl[0] + d[0] * norm, 
-				saved[1] + desl[1] + d[1] * norm, 
-				saved[2] + desl[2] + d[2] * norm,
+	distance_to_lookat[0] = d[0] * norm,
+	distance_to_lookat[1] = d[1] * norm,
+	distance_to_lookat[2] = d[2] * norm;
+}
+
+void save_position() {
+	float desl[3];
+	float dist[3];
+
+	calculate_displacement(desl, dist);
+
+	last_camera_position[0] += desl[0];
+	last_camera_position[1] += desl[1];
+	last_camera_position[2] += desl[2];
+	camera_front = 0;
+	camera_up = 0;
+	camera_side = 0;
+}
+
+void renderScene(void) {
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// set the camera
+	glLoadIdentity();
+
+	float desl[3];
+	float dist[3];
+	calculate_displacement(desl, dist);
+
+	//os c�lculos est�o aqui dentro se por ventura nos interessar mudar a dire��o da c�mera (implica recalcular)
+	//se estiver demasiado lento p�e-se no in�cio
+
+	gluLookAt(	desl[0] + last_camera_position[0], desl[1] + last_camera_position[1], desl[2] + last_camera_position[2],
+				last_camera_position[0] + desl[0] + dist[0], 
+				last_camera_position[1] + desl[1] + dist[1], 
+				last_camera_position[2] + desl[2] + dist[2],
 				camera_global->up[0], camera_global->up[1], camera_global->up[2]);
 
 	// Colocar funcoes de desenho aqui
@@ -208,7 +197,7 @@ void processKeys(unsigned char key, int xx, int yy) {
 		camera_side = 0;
 		camera_up = 0;
 		camera_front = 0;
-		saved[0] = camera_global->position[0], saved[1] = camera_global->position[1], saved[2] = camera_global->position[2];
+		last_camera_position[0] = camera_global->position[0], last_camera_position[1] = camera_global->position[1], last_camera_position[2] = camera_global->position[2];
 		break;
 	}
 
@@ -298,9 +287,9 @@ void processSpecialKeys(int key, int xx, int yy) {
 void run(Window* window, Camera* camera, Group* group, int argc, char* argv[]) {
     camera_global = camera;
 	group_global = group;
-	saved[0] = camera_global->position[0];
-	saved[1] = camera_global->position[1];
-	saved[2] = camera_global->position[2];
+	last_camera_position[0] = camera_global->position[0];
+	last_camera_position[1] = camera_global->position[1];
+	last_camera_position[2] = camera_global->position[2];
 
 	// init GLUT and #include "parser.cpp"the window
 	glutInit(&argc, argv);
