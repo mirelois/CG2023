233c30351bfbcfea648764e981de50557fda477e
André Lucena Ferreira <69969391+Lucenix@users.noreply.github.com>
2023-05-01 00:42:21 +0100

    Update parser.h

--- src/engine/parser.h
+++ src/engine/parser.h
@@ -9,5 +9,7 @@
 using namespace std;
 using namespace rapidxml;
 
+
+void parser(char* fileName, Window* window, Camera* camera, Group* group, vector<float>* points);
 void parse_group(xml_node<>* group_node, Group* group, vector<float>* points,
     unordered_map<string, Model*>* model_map, int* index);
