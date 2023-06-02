92acba1fbbaa3ccb2122d6c037e93861196ba01f
mirelois <79458822+mirelois@users.noreply.github.com>
2023-06-02 15:51:17 +0100

    breaking production

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
@@ -13,22 +14,28 @@
 
 using namespace std;
 
-tuple<float *, unsigned int *>
+tuple<float *, float *, unsigned int *>
 generate_torus_index(float inner_radius, float outer_radius,
                      int vertical_divisions, int horizontal_divisions,
-                     unsigned int *points_total, unsigned int *index_total) {
+                     unsigned int *points_total, unsigned int *index_total, unsigned int *normal_total) {
 
     *points_total = horizontal_divisions * vertical_divisions * 3;
+    *normal_total = horizontal_divisions * vertical_divisions * 3;
     *index_total = 6 * vertical_divisions * horizontal_divisions;
 
     float *point_array = (float *)malloc(sizeof(float) * *points_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *points_total);
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
     float *master_circle =
         (float *)malloc(sizeof(float) * (vertical_divisions * 3));
+    float *master_circle_normal =
+        (float *)malloc(sizeof(float) * (vertical_divisions * 3));
     int index = 0;
+    int index_normal = 0;
 
     int master_circle_index = 0;
+    int master_circle_index_normal = 0;
 
     float delta_x = 2 * M_PI / vertical_divisions;
 
@@ -45,6 +52,10 @@ generate_torus_index(float inner_radius, float outer_radius,
         master_circle[master_circle_index++] = pivot_y * cos(i * delta_x);
         master_circle[master_circle_index++] =
             pivot_y * sin(i * delta_x) + (outer_radius + inner_radius) / 2;
+        
+        master_circle_normal[master_circle_index_normal++] = 0;
+        master_circle_normal[master_circle_index_normal++] = cos(i * delta_x);
+        master_circle_normal[master_circle_index_normal++] = sin(i * delta_x);
     }
 
     for (int j = 0; j < vertical_divisions; j++) {
@@ -56,6 +67,14 @@ generate_torus_index(float inner_radius, float outer_radius,
             point_array[index++] =
                 (master_circle[3 * j + 0]) * sin(i * delta_y) +
                 (master_circle[3 * j + 2]) * cos(i * delta_y);
+            
+            normal_array[index_normal++] =
+                (master_circle_normal[3 * j + 0]) * cos(i * delta_y) +
+                (master_circle_normal[3 * j + 2]) * sin(i * delta_y);
+            normal_array[index_normal++] = (master_circle_normal[3 * j + 1]);
+            normal_array[index_normal++] =
+                (master_circle_normal[3 * j + 0]) * sin(i * delta_y) +
+                (master_circle_normal[3 * j + 2]) * cos(i * delta_y);
         }
     }
 
@@ -79,17 +98,19 @@ generate_torus_index(float inner_radius, float outer_radius,
         }
     }
 
-    return make_tuple(point_array, index_array);
+    return make_tuple(point_array, normal_array, index_array);
 }
 
-tuple<float *, unsigned int *>
+tuple<float *, float *, unsigned int *>
 generate_cone_index(float bottom_radius, float height, int slices, int stacks,
-                    unsigned int *point_total, unsigned int *index_total) {
+                    unsigned int *point_total, unsigned int *index_total, unsigned int *normal_total) {
 
     *point_total = 3 * ((slices + 1) * stacks + 2);
+    *normal_total = 3 * ((slices + 1) * stacks + 2);
     *index_total = slices * stacks * 6;
 
     float *point_array = (float *)malloc(sizeof(float) * *point_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *point_total);
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
 
@@ -97,12 +118,24 @@ generate_cone_index(float bottom_radius, float height, int slices, int stacks,
     double alfa = 2 * M_PI / slices;
     double division_height_step = height / stacks;
     double division_radius_step = bottom_radius / stacks;
+    
+    double norm = sqrt(pow(bottom_radius, 2) + pow(height, 2));
+
+    //float normalx = 0;
+    float normaly = bottom_radius/norm;
+    float normalz = height/norm;
+
     int index = 0;
+    int index_normal = 0;
 
     // top point
     point_array[index++] = 0;
     point_array[index++] = height;
     point_array[index++] = 0;
+    
+    normal_array[index_normal++] = 0;
+    normal_array[index_normal++] = 1;
+    normal_array[index_normal++] = 0;
 
     for (i = 0; i < stacks; i++) {
         double sub_height = height - division_height_step * (i + 1);
@@ -113,6 +146,10 @@ generate_cone_index(float bottom_radius, float height, int slices, int stacks,
             point_array[index++] = sub_radius * sin(alfa * j);
             point_array[index++] = sub_height;
             point_array[index++] = sub_radius * cos(alfa * j);
+
+            normal_array[index_normal++] = normalz * sin(alfa * j);
+            normal_array[index_normal++] = normaly;
+            normal_array[index_normal++] = normalz * cos(alfa * j);
         }
     }
 
@@ -122,11 +159,15 @@ generate_cone_index(float bottom_radius, float height, int slices, int stacks,
         point_array[index++] = bottom_radius * sin(alfa * j);
         point_array[index++] = 0;
         point_array[index++] = bottom_radius * cos(alfa * j);
+    
+        normal_array[index_normal++] = 0;
+        normal_array[index_normal++] = -1;
+        normal_array[index_normal++] = 0;
     }
 
     // bottom point
     point_array[index++] = 0;
-    point_array[index++] = 0;
+    point_array[index++] = -1;
     point_array[index++] = 0;
 
     index = 0;
@@ -156,17 +197,19 @@ generate_cone_index(float bottom_radius, float height, int slices, int stacks,
         index_array[index++] = offset + (i + 1) % slices;
     }
 
-    return make_tuple(point_array, index_array);
+    return make_tuple(point_array, normal_array, index_array);
 }
 
-tuple<float *, unsigned int *>
+tuple<float *, float *, unsigned int *>
 generate_cylinder_index(float radius, float height, int slices, int stacks,
-                        unsigned int *point_total, unsigned int *index_total) {
+                        unsigned int *point_total, unsigned int *index_total, unsigned int *normal_total) {
 
     *point_total = 3 * ((slices + 2) * (stacks + 1) + 2);
+    *normal_total = 3 * ((slices + 2) * (stacks + 1) + 2);
     *index_total = slices * ((stacks + 1) * 6);
 
     float *point_array = (float *)malloc(sizeof(float) * *point_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *point_total);
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
 
@@ -174,17 +217,26 @@ generate_cylinder_index(float radius, float height, int slices, int stacks,
     float alfa = 2 * M_PI / slices;
 
     int index = 0;
+    int index_normal = 0;
 
     // top point
     point_array[index++] = 0.0f;
     point_array[index++] = height / 2;
     point_array[index++] = 0.0f;
     
+    normal_array[index_normal++] = 0;
+    normal_array[index_normal++] = 1;
+    normal_array[index_normal++] = 0;
+    
     for (int i = 0; i < slices; i++) {
         // top ring
         point_array[index++] = radius * sin(alfa * i);
         point_array[index++] = height/2;
         point_array[index++] = radius * cos(alfa * i);
+    
+        normal_array[index_normal++] = 0;
+        normal_array[index_normal++] = 1;
+        normal_array[index_normal++] = 0;
     }
 
     for (int j = 0; j < stacks + 1; j++) {
@@ -195,6 +247,10 @@ generate_cylinder_index(float radius, float height, int slices, int stacks,
             point_array[index++] = radius * sin(alfa * i);
             point_array[index++] = sub_height;
             point_array[index++] = radius * cos(alfa * i);
+
+            normal_array[index_normal++] = sin(alfa * i);
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = cos(alfa * i);
         }
     }
     
@@ -203,6 +259,10 @@ generate_cylinder_index(float radius, float height, int slices, int stacks,
         point_array[index++] = radius * sin(alfa * i);
         point_array[index++] = -height/2;
         point_array[index++] = radius * cos(alfa * i);
+    
+        normal_array[index_normal++] = 0;
+        normal_array[index_normal++] = -1;
+        normal_array[index_normal++] = 0;
     }
 
     // bottom point
@@ -210,6 +270,11 @@ generate_cylinder_index(float radius, float height, int slices, int stacks,
     point_array[index++] = -height / 2;
     point_array[index++] = 0.0f;
 
+    
+    normal_array[index_normal++] = 0;
+    normal_array[index_normal++] = -1;
+    normal_array[index_normal++] = 0;
+    
     index = 0;
 
     for (int i = 0; i < slices; i++) {
@@ -239,29 +304,28 @@ generate_cylinder_index(float radius, float height, int slices, int stacks,
         index_array[index++] = offset + (i + 1) % slices;
     }
 
-    for(int i=0; i<*index_total; i++){
-        printf("%u,", index_array[i]);
-        if((i+1)%3 == 0) putchar('\n');
-    }
-
-    return make_tuple(point_array, index_array);
+    return make_tuple(point_array, normal_array, index_array);
 }
 
-tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
+tuple<float *, float *, unsigned int *> generate_box_index(float length, int grid_slices,
                                                   unsigned int *points_total,
-                                                  unsigned int *index_total) {
+                                                  unsigned int *index_total,
+                                                  unsigned int *normal_total) {
     // a quantidade de pontos ´e definida pelo grid;
     // agora tem de ser pontos com normais diferentes por isso ´e por face
     *points_total = (grid_slices+1)*(grid_slices+1) * 3 * 6;
+    *normal_total = (grid_slices+1)*(grid_slices+1) * 3 * 6;
     *index_total = grid_slices * grid_slices * 36;
 
     float *point_array = (float *)malloc(sizeof(float) * *points_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *points_total);
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
 
     float delta = length / grid_slices;
 
     int index = 0;
+    int index_normal = 0;
 
     float referential_x = -length / 2;
 
@@ -277,6 +341,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = j * delta + referential_x;
             point_array[index++] = -i * delta + referential_y;
             point_array[index++] = referential_z;
+            
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 1;
         }
     }
     for (int i = 0; i < grid_slices + 1; i++) {
@@ -286,6 +354,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = referential_z;
             point_array[index++] = -i * delta + referential_y;
             point_array[index++] = -(j * delta + referential_x);
+            
+            normal_array[index_normal++] = 1;
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 0;
         }
     }
     for (int i = 0; i < grid_slices + 1; i++) {
@@ -295,6 +367,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = -(j * delta + referential_x);
             point_array[index++] = -i * delta + referential_y;
             point_array[index++] = -referential_z;
+            
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = -1;
         }
     }
     for (int i = 0; i < grid_slices + 1; i++) {
@@ -304,6 +380,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = -referential_z;
             point_array[index++] = -i * delta + referential_y;
             point_array[index++] = j * delta + referential_x;
+            
+            normal_array[index_normal++] = -1;
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 0;
         }
     }
 
@@ -314,6 +394,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = j * delta + referential_x;
             point_array[index++] = -referential_z;
             point_array[index++] = -i * delta + referential_y;
+            
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 1;
+            normal_array[index_normal++] = 0;
         }
     }
     
@@ -323,6 +407,10 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
             point_array[index++] = j * delta + referential_x;
             point_array[index++] = referential_z;
             point_array[index++] = i * delta - referential_y;
+            
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = -1;
+            normal_array[index_normal++] = 0;
         }
     }
 
@@ -343,24 +431,29 @@ tuple<float *, unsigned int *> generate_box_index(float length, int grid_slices,
         }
     }
 
-    return make_tuple(point_array, index_array);
+    return make_tuple(point_array, normal_array, index_array);
 }
 
-tuple<float *, unsigned int *> generate_plane_index(float length,
+tuple<float *,float *, unsigned int *> generate_plane_index(float length,
                                                     int grid_slices,
                                                     unsigned int *points_total,
-                                                    unsigned int *index_total) {
+                                                    unsigned int *index_total,
+                                                    unsigned int *normal_total) {
 
     *index_total = grid_slices * grid_slices * 6;
     *points_total = (grid_slices + 1) * (grid_slices + 1) * 3;
+    *normal_total = (grid_slices + 1) * (grid_slices + 1) * 3;
 
     float *point_array = (float *)malloc(sizeof(float) * *points_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *points_total);
+    
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
 
     float delta = length / grid_slices;
 
     int index = 0;
+    int index_normal = 0;
 
     float referential_x = -length / 2;
 
@@ -371,6 +464,10 @@ tuple<float *, unsigned int *> generate_plane_index(float length,
             point_array[index++] = j * delta + referential_x;
             point_array[index++] = 0.0f;
             point_array[index++] = i * delta + referential_z;
+            
+            normal_array[index_normal++] = 0;
+            normal_array[index_normal++] = 1.0f;
+            normal_array[index_normal++] = 0;
         }
     }
 
@@ -388,14 +485,16 @@ tuple<float *, unsigned int *> generate_plane_index(float length,
             index_array[index++] = (grid_slices + 1) * i + (j + 1);
         }
     }
-    return make_tuple(point_array, index_array);
+    return make_tuple(point_array, normal_array, index_array);
 }
 
-tuple<float *, unsigned int *>
+tuple<float *, float *, unsigned int *>
 generate_sphere_index(float radius, int slices, int stacks,
-                      unsigned int *points_total, unsigned int *index_total) {
+                      unsigned int *points_total, unsigned int *index_total, unsigned int *normal_total) {
     *index_total = slices * 6 * (stacks - 1);
     *points_total = 3 * (slices * (stacks - 1) + 2);
+    *normal_total = 3 * (slices * (stacks - 1) + 2);
+    
     int master_line_size = (stacks + 1) * 3;
     float alfa_x = M_PI / stacks;
     float alfa_y = 2 * M_PI / slices;
@@ -405,22 +504,35 @@ generate_sphere_index(float radius, int slices, int stacks,
     // float pivot_z = 0;
 
     float *master_line = (float *)malloc(sizeof(float) * master_line_size);
+    float *master_line_normal = (float *)malloc(sizeof(float) * master_line_size);
     float *points_array = (float *)malloc(sizeof(float) * *points_total);
+    float *normal_array = (float *)malloc(sizeof(float) * *points_total);
     unsigned int *index_array =
         (unsigned int *)malloc(sizeof(unsigned int) * *index_total);
 
     int master_line_index = 0;
+    int master_line_index_normal = 0;
     for (int i = 0; i < stacks + 1; i++) {
         master_line[master_line_index++] = pivot_x;
         master_line[master_line_index++] = pivot_y * cos(i * alfa_x);
         master_line[master_line_index++] = pivot_y * sin(i * alfa_x);
+        
+        master_line_normal[master_line_index_normal++] = 0;
+        master_line_normal[master_line_index_normal++] = cos(i * alfa_x);
+        master_line_normal[master_line_index_normal++] = sin(i * alfa_x);
     }
 
     int index = 0;
+    int index_normal = 0;
 
+    //top point
     points_array[index++] = master_line[0];
     points_array[index++] = master_line[1];
     points_array[index++] = master_line[2];
+    
+    normal_array[index++] = 0;
+    normal_array[index++] = 1;
+    normal_array[index++] = 0;
 
     for (int j = 0; j < slices; j++) {
         for (int i = 1; i <= stacks - 1; i++) {
@@ -429,12 +541,23 @@ generate_sphere_index(float radius, int slices, int stacks,
             points_array[index++] = (master_line[i * 3 + 1]);
             points_array[index++] = (master_line[i * 3 + 0]) * sin(j * alfa_y) +
                                     (master_line[i * 3 + 2]) * cos(j * alfa_y);
+            
+            normal_array[index_normal++] = (master_line_normal[i * 3 + 0]) * cos(j * alfa_y) +
+                                    (master_line_normal[i * 3 + 2]) * sin(j * alfa_y);
+            normal_array[index_normal++] = (master_line_normal[i * 3 + 1]);
+            normal_array[index_normal++] = (master_line_normal[i * 3 + 0]) * sin(j * alfa_y) +
+                                    (master_line_normal[i * 3 + 2]) * cos(j * alfa_y);
         }
     }
 
+    //bottom point
     points_array[*points_total - 3] = master_line[master_line_size - 1];
     points_array[*points_total - 2] = master_line[master_line_size - 2];
     points_array[*points_total - 1] = master_line[master_line_size - 3];
+    
+    normal_array[index_normal++] = 0;
+    normal_array[index_normal++] = -1;
+    normal_array[index_normal++] = 0;
 
     index = 0;
 
@@ -464,7 +587,7 @@ generate_sphere_index(float radius, int slices, int stacks,
         index_array[index++] = (stacks - 1) + (stacks - 1) * ((j + 1) % slices);
     }
 
-    return make_tuple(points_array, index_array);
+    return make_tuple(points_array, normal_array, index_array);
 }
 
 void write3D(const char *filename, unsigned int nVertices, float *points,
